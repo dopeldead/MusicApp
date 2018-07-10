5a1338926ca5cd796fb090c3e41fb2811a64cb27
dopeldead <gidon@intechinfo.fr>
2018-07-10 15:42:20 +0200

    added artist DB

--- app/src/main/java/iti/musicapp/models/User.kt
+++ app/src/main/java/iti/musicapp/models/User.kt
@@ -1,28 +1,31 @@
 package iti.musicapp.models
 import java.time.LocalDateTime
 
-class User() {
-
-    var artists= mutableListOf<Artist>()
+class User(val artistsDb: Set<Artist>) {
+    private var favoriteArtists = mutableListOf<Artist>()
     private var styles = mutableMapOf<Style,LocalDateTime>()
 
     fun AddFavorite(artist: Artist){
-        if(artist !in artists) artists.add(artist)
+        if(artist !in favoriteArtists) favoriteArtists.add(artist)
         for (s in artist.styles){
             if(s !in styles.keys) styles.put(s, LocalDateTime.now())
         }
     }
-
-    fun getSuggestions(collection: AbstractCollection<Artist>): List<Artist>{
+    fun AddFavorite(artistName: String){
+        val artist = artistsDb.find { it.name==artistName }
+        if(artist == null) throw Exception("The artist name should Exist")
+        AddFavorite(artist)
+    }
+    fun getSuggestions(): List<Artist>{
 
         val preferedStyles =  styles.keys
-                .map {Pair(it,artists.count { a -> a.styles.contains(it) }) }
+                .map {Pair(it,favoriteArtists.count { a -> a.styles.contains(it) }) }
                 .sortedWith(
                         compareByDescending<Pair<Style,Int>>{ it.second }
                                 .thenBy { styles[it.first] }
                 )
                 .take(2)
 
-        return collection.filter { a-> a.styles.intersect(preferedStyles).count()!=0 }.filter { it !in artists }
+        return artistsDb.filter { a-> a.styles.intersect(preferedStyles).count()!=0 }.filter { it !in favoriteArtists }
     }
 }
